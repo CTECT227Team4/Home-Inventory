831223b22338c5d726bcaf82adf5f9e3abd08005
Lynne Rochelle <l.rochelle@students.clark.edu>
2015-06-06 13:43:32 -0700

    validation

---
+++ js/validate_password_change.js
@@ -0,0 +1,100 @@
+		$(document).ready(function(){
+			$(".error").hide();  // hide the errors, if any exist
+
+			var value = "";   // create variables
+			var passwordValue = "";
+       		var verifyValue = "";
+			var errorCurrent = false;
+			var errorPassword = false;
+			var errorVerify = false;
+
+			var preventSubmit = true;
+
+		//This section checks for empty fields as the form is being filled out 
+
+			$(':input#currentPassword').blur(function(){    // check when cursor moves out of field that it isn't empty
+					value = $(this).val();
+					if (value == "") {                // if the value is empty
+						$(this).addClass('error');    // turns on the error class
+						$('#currentError').show();      // shows the error message 
+					} else {
+						$(this).removeClass('error'); // turns off the error class
+						$('#currentError').hide();      // turns off the error message
+					} // end if
+			});//end each
+
+			$(':input#password').blur(function(){    // check when cursor moves out of field that it isn't empty
+					passwordValue = $(this).val();
+					if (passwordValue == "") {                // if the value is empty
+						$(this).addClass('error');    // turns on the error class
+						$('#passwordError').show();      // shows the error message 
+					} else {
+						$(this).removeClass('error'); // turns off the error class
+						$('#passwordError').hide();      // turns off the error message
+					} // end if
+			});//end each
+
+			$(':input#verifyPassword').blur(function(){    // check when cursor moves out of field that it isn't empty
+					verifyValue = $(this).val();
+					if (verifyValue == "") {                // if the value is empty
+						$(this).addClass('error');    // turns on the error class
+						$('#verifyError').show();      // shows the error message 
+        			} else if (verifyValue != passwordValue) {
+						$(this).addClass('error');    // turns on the error class
+						$('#verifyError2').show();      // shows the error message 
+					} else {
+						$(this).removeClass('error'); // turns off the error class
+						$('#verifyError').hide();      // turns off the error message
+						$('#verifyError2').hide();     // turns off the 2nd error message
+					} // end if
+			});//end each
+
+		//  This section is a function that checks data and returns a true/false to prevent submit 
+		function checkData() {
+			$(':input#firstName').focus();  // Moves through all of the fields to trigger errors
+			$(':input#lastName').focus();
+			$(':input#email').focus();
+			$(':input#userName').focus();
+			$(':input#password').focus();
+			$(':input#verifyPassword').focus();
+
+			// Now it's checking for errors and returns true to preventSubmit
+			value = $(':input#firstName').val();
+			if ( value == "" ){
+				return true;
+			};
+			value = $(':input#lastName').val();
+			if ( value == "" ) {
+				return true;
+			};
+			value = $(':input#email').val();
+			if ( value == "" ) {
+				return true;
+			};
+			value = $(':input#userName').val();
+			if ( value == "" ) {
+				return true;
+			};
+			value = $(':input#password').val();
+			if ( value == "" ) {
+				return true;
+			};
+			value = $(':input#verifyPassword').val();
+			if ( value == "" ) {
+				return true;
+			};
+		};// end checkData()
+
+
+		//  This function handled the submit button
+			$('#register').submit(function(event){
+				$('#register').prop('disabled',true);     	// disable submit button
+
+				preventSubmit = checkData();     			// sets a variable to the value returned from the checkData function 
+
+				if (preventSubmit == true) {
+					event.preventDefault();
+					$('#register').prop('disabled',false);    	// re-enable submit button
+				};  // enter re-enable submit
+			});//end submit
+		}); // end ready
