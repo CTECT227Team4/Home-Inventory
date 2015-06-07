c5046dc8b327b57a649e5640ee3d9dbb19cd5cdc
Rosemary <perkinsrosemaryl@gmail.com>
2015-06-07 16:05:43 -0700

    edit_user.php outputs user's properties

--- edit_user.php
+++ edit_user.php
@@ -7,6 +7,7 @@ $nav_context = "inventory";
 
 require_once "inc/header.inc.php";
 
+$id = $_GET["id"];
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
 	$id = $_GET["id"];
@@ -26,9 +27,11 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
 	<div>
 	<br><br><br>
-		<h2>Edit User</h2>
+		<h2>Edit User <?php echo $id; ?></h2>
+	<div class="users_table">
 
 		<table>
+			<tr><th colspan="6"><h3>User Info</h3></th></tr>
 			<tr>
 				<th>ID</th>
 				<th>Username</th>
@@ -38,7 +41,6 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 				<th>User Type</th>
 			</tr>
 			<?php
-				$id = $_GET["id"];
 				$sql = "SELECT id, userName, firstName, lastName, email, usertypeID FROM user WHERE id=$id";
 				$parameters = [$id];
 
@@ -52,6 +54,40 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 				}
 			?>
 		</table>
+	</div>
+
+	<hr class="border">
+
+	<div class="users_table">
+
+		<table>
+			<tr><th colspan="5"><h3>Properties</h3></th></tr>
+			<tr>
+				<th>Property ID</th>
+				<th>Name</th>
+				<th>Address</th>
+				<th>Zip Code</th>
+				<th>Description</th>
+			</tr>
+			<?php
+
+			$sql = "SELECT propertyID, name, address, zip, description FROM property p INNER JOIN user_property up ON p.ID = up.propertyID INNER JOIN user u ON u.ID = up.userID WHERE up.userID = ?";
+
+				$parameters = [$id];
+
+				$properties = getRecordset($con,$sql,$parameters);
+				foreach ($properties as $property) {
+					echo "<tr>";
+					foreach ($property as $property_info) {
+						echo "<td>" . $property_info . "</td>";
+					}
+					echo "</tr>";
+				}
+
+
+			 ?>
+		</table>
+		</div>
 
 		<form action="edit_user.php?id=<?php echo $id; ?>" method="POST">
 			<fieldset>
